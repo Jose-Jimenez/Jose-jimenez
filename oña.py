a894f07363d30aa5d788e254e86a6eda128d1079
Jose-Jimenez <56984604+Jose-Jimenez@users.noreply.github.com>
2019-10-25 18:32:00 -0500

    Add files via upload

---
+++ oña.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction to Python
+
+# ## Excersice 1: A Good First Program
+# 
+
+# 
+
+# In[1]:
+
+
+print("Hello World")
+print("*,-,/,+")
+print("put me an AU please")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
